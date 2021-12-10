282d43c73aa3fafe137270604a50f89a28c62681
dviroshere <78240166+dviroshere@users.noreply.github.com>
2021-12-10 19:33:19 +0200

    Update script.js

--- script.js
+++ script.js
@@ -77,6 +77,7 @@ const buyBike = function (value){
 
 window.location.href === 'https://dviroshere.github.io/BikeRo/buyBike.html' ? buyBike() : '';
 
+const aboutBtn = function(value){
 btnAbout.addEventListener('click', function (e) {
   e.preventDefault();
 
@@ -84,3 +85,7 @@ btnAbout.addEventListener('click', function (e) {
 
   id.scrollIntoView({ behavior: 'smooth' });
 });
+}
+
+window.location.href === 'https://dviroshere.github.io/BikeRo/' ? aboutBtn() : '';
+
